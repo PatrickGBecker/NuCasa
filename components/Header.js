46ef7441865887d1dd339cfe2a93b463cd329eaa
Patrick Becker <patrickgbecker@gmail.com>
2023-02-16 14:50:03 -0600

    refactor

--- components/Header.js
+++ components/Header.js
@@ -52,7 +52,7 @@ function Header({ placeholder }) {
     grid-cols-3 bg-[#74a7af] shadow-md  px-5 md:px-10'>
 
         {/* left */}
-        <div onClick={() => router.push('/')} 
+        {/*<div onClick={() => router.push('/')} 
         className='relative flex items-center h-20 ml-8
         cursor-pointer my-auto'>
             <div className='mt-5 sm:mt-10 sm:mr-8 md:mt-12'>
@@ -63,7 +63,7 @@ function Header({ placeholder }) {
                     className='object-contain object-left'
                 />
             </div>
-        </div>
+  </div>*/}
         {/* middle */}
         <div className='flex items-center h-10 mt-5 md:border-2 rounded-full py-2 
         md:shadow-sm'>
@@ -78,15 +78,15 @@ function Header({ placeholder }) {
              text-white rounded-full p-2 cursor-pointer md:mx-2 ' />
         </div>
         {/* right */}
-        <div className='flex items-center space-x-4 justify-end text-white'>
+        {/*<div className='flex items-center space-x-4 justify-end text-white'>
             <p className='hidden md:inline cursor-pointer'>Become a Host</p>
             <GlobeAltIcon className='h-6 cursor-pointer'/>
 
             <div className='flex items-center space-x-2 border-2 p-2 rounded-full'>
                 <MenuIcon className='h-6' />
                 <UserCircleIcon className='h-6' />
-            </div>
-        </div>
+  </div>*/}
+        
         
         {searchInput && (
             <div className='flex flex-col col-span-3 mx-auto pb-2'>
